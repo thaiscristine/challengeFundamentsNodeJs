cd60b6063949c3c25ccb70582347b11bc005ceba
Thais Cristine <this.cristine06@gmail.com>
2020-11-13 21:28:09 +0200

    feat: adding repository and service of create route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
+import Transaction from '../models/Transaction';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    return response.json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +19,15 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    createTransaction.execute({ title, value, type });
+
+    return response.json(createTransaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
